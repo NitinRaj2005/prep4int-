23d0f24218ef56a367b36ef88bf913bb8a4cccc1
Nitin Raj <itsmeni3raj21@gmail.com>
2025-08-14 17:54:52 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb+srv://Amansingh:amansingh245172@cluster.za7ia9z.mongodb.net/reg", {useNewUrlParser : true, useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://Nitin:nitin-123@cluster0.s2jvkgz.mongodb.net/", {useNewUrlParser : true, useUnifiedTopology: true})
 .then(()=>console.log("connection success"))
 .catch((err)=> console.log(err));
